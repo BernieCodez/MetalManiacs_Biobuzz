4c02bbdf7c8615a49bde667ecfa2744c460b9e04
skullblazed <skullblazed2@gmail.com>
2026-09-22 18:55:18 -0700

    V1.8.1 Tweaked positions a little bit and added wait
    Added wait to stop bot while outtake and intake is going on. Changes positions so the bot doesn't hit the wall

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/GauranshAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/GauranshAuto.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.auto;
+package org.firstinspires.ftc.teamcode;
 
 import static com.pedropathing.api.Paths.*;
 
@@ -25,37 +25,41 @@ public class GauranshAuto extends LinearOpMode {
 
     private final PoseFactory poseFactory = PoseFactory.degrees();
 
-    private final Pose start = poseFactory.of(56, 8, 90);
-    private final Pose path1Start = poseFactory.of(56, 8, 0);
-    private final Pose path1 = poseFactory.of(59.3842, 13.157, 90);
-    private final Pose point2 = poseFactory.of(8.7182, 8.9933, -90);
-    private final Pose point2Control1 = poseFactory.of(28.58, 15.9342, 0);
-    private final Pose point2Segment1Start = poseFactory.of(8.7182, 8.9933, 90);
-    private final Pose point2Segment1End = poseFactory.of(8.7182, 8.9933, -90);
-    private final Pose point3 = poseFactory.of(57.9283, 126.3348, -90);
-    private final Pose point4 = poseFactory.of(58.3569, 126.5206, -90);
-    private final Pose point4Segment1Start = poseFactory.of(58.3569, 126.5206, -90);
-    private final Pose point4Segment1End = poseFactory.of(58.3569, 126.5206, -90);
+    private final Pose start = poseFactory.of(56.6345, 10.5381, 90);
+    private final Pose path1 = poseFactory.of(59.3842, 13.157, 0);
+    private final Pose point2 = poseFactory.of(59.3842, 13.157, 90);
+    private final Pose point2Segment1Heading = poseFactory.of(59.3842, 13.157, 90);
+    private final Pose point3 = poseFactory.of(11.0448, 12.1659, -90);
+    private final Pose point3Control1 = poseFactory.of(33.4447, 37.2967, 0);
+    private final Pose point3Segment1Start = poseFactory.of(11.0448, 12.1659, 90);
+    private final Pose point3Segment1End = poseFactory.of(11.0448, 12.1659, -90);
+    private final Pose point4 = poseFactory.of(60.2549, 124.0082, -90);
     private final Pose point5 = poseFactory.of(47.2765, 131.3797, -270);
     private final Pose point5Control1 = poseFactory.of(49.7511, 113.0284, 0);
     private final Pose point5Segment1Start = poseFactory.of(47.2765, 131.3797, -90);
     private final Pose point5Segment1End = poseFactory.of(47.2765, 131.3797, 90);
-    private final Pose point6 = poseFactory.of(58.3438, 126.6472, 270);
+    private final Pose point6 = poseFactory.of(59.6129, 124.5321, 270);
     private final Pose point6Control1 = poseFactory.of(49.6981, 112.6532, 0);
-    private final Pose point6Segment1Start = poseFactory.of(58.3438, 126.6472, 90);
-    private final Pose point6Segment1End = poseFactory.of(58.3438, 126.6472, -90);
-    private final Pose point7 = poseFactory.of(56.5426, 10.8333, 131.7671);
+    private final Pose point6Segment1Start = poseFactory.of(59.6129, 124.5321, 90);
+    private final Pose point6Segment1End = poseFactory.of(59.6129, 124.5321, -90);
+    private final Pose point7 = poseFactory.of(57.1771, 12.3139, 0);
     private final Pose point7Control1 = poseFactory.of(12.6465, 59.3901, 0);
+    private final Pose point7Segment1Start = poseFactory.of(57.1771, 12.3139, -90);
+    private final Pose point7Segment1End = poseFactory.of(57.1771, 12.3139, 0);
 
     // Autonomous routine
     public Command autoRoutine() {
         return sequential(
                 follow(follower, path1()),
                 follow(follower, path2()),
+                waitMs(2000),
                 follow(follower, path3()),
                 follow(follower, path4()),
+                waitMs(2000),
                 follow(follower, path5()),
+                waitMs(3500),
                 follow(follower, path6()),
+                waitMs(2000),
                 follow(follower, path7())
         );
     }
@@ -88,19 +92,19 @@ public class GauranshAuto extends LinearOpMode {
     }
 
     public Path path1() {
-        return line(path1Start, path1).linear(path1Start, path1);
+        return line(start, path1).constant(path1);
     }
 
     public Path path2() {
-        return curve(path1, point2Control1, point2).heading(Interpolator.piecewise().until(1, Interpolator.linear(point2Segment1Start, point2Segment1End).reverse()));
+        return line(path1, point2).heading(Interpolator.piecewise().until(1, Interpolator.constant(point2Segment1Heading)));
     }
 
     public Path path3() {
-        return line(point2, point3).linear(point2, point3);
+        return curve(point2, point3Control1, point3).heading(Interpolator.piecewise().until(1, Interpolator.linear(point3Segment1Start, point3Segment1End).reverse()));
     }
 
     public Path path4() {
-        return line(point3, point4).heading(Interpolator.piecewise().until(1, Interpolator.linear(point4Segment1Start, point4Segment1End)));
+        return line(point3, point4).linear(point3, point4);
     }
 
     public Path path5() {
@@ -112,6 +116,6 @@ public class GauranshAuto extends LinearOpMode {
     }
 
     public Path path7() {
-        return curve(point6, point7Control1, point7).reverseTangent();
+        return curve(point6, point7Control1, point7).heading(Interpolator.piecewise().until(1, Interpolator.linear(point7Segment1Start, point7Segment1End)));
     }
 }
